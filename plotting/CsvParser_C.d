bda081a4be9a1709e584ca38f2103cb127b86161
OJevons <oliver.jevons@glasgow.ac.uk>
2023-06-07 16:27:42 +0100

    cleaned up folders

--- plotting/CsvParser_C.d
+++
@@ -1,6 +0,0 @@
-
-# DO NOT DELETE
-
-./CsvParser_C.so: CsvParser.h
-./CsvParser_C.so: /mnt/share/cern_CentOS7/root/v6.20.04/include/RVersion.h /mnt/share/cern_CentOS7/root/v6.20.04/include/RConfig.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TClass.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TDictAttributeMap.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TInterpreter.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TROOT.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TBuffer.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TMemberInspector.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TError.h /mnt/share/cern_CentOS7/root/v6.20.04/include/RtypesImp.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TIsAProxy.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TFileMergeInfo.h /mnt/share/cern_CentOS7/root/v6.20.04/include/TCollectionProxyInfo.h /cern/root/v6.20.04/bin/rootcling
-CsvParser_C__ROOTBUILDVERSION= 6.20/04
